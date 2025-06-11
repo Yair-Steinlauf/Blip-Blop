2f83be1d064b39ab15f2a69febd0ea7950bc8ce0
Yair-Steinlauf <yair.steinlauf@gmail.com>
2025-06-11 17:14:29 +0300

    added box2d

--- include/Entitys/BaseEntity.h
+++ include/Entitys/BaseEntity.h
@@ -1,4 +1,9 @@
 #pragma once
+// Disable MSVC analysis warnings for the box2d include
+#pragma warning(push)
+#pragma warning(disable: 26495 26813)
+#include "box2d/box2d.h"
+#pragma warning(pop)
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include "Factory.h"
